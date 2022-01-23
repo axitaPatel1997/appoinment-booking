8add2e9f9d2f8cfdcef9b1d40ae62e98a35d8dd2
Ali Waris <90821209+axitaPatel1997@users.noreply.github.com>
2022-01-23 10:37:34 +0530

    [config] setup swift project.

---
+++ Appoinment Booking/Appoinment Booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appoinment Booking
+//
+//  Created by MOB-090821-014 on 23/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
